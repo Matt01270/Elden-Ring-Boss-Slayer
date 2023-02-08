ae20d862bdaf4ce5eaf13487864e27930f76db05
Matt01270 <matt.92.online@googlemail.com>
2023-02-08 13:57:20 -0500

    improved page styling and added background

--- src/App.js
+++ src/App.js
@@ -63,11 +63,13 @@ useEffect(() => {
 
   return ( 
     <div className="App">  
+    <div className="wrapper">
       <h1>ELDEN RING</h1> 
       <h2>Boss Slayer</h2>  
       <div className="introText"><p>Hail, Tarnished. This tool shall aid thee in thy journey to vanquish each and every monstrous overlord that roams the Lands Between. May thy luck be ever strong.</p></div> 
       <ProgressBar changedBossCount={changedBossCount}/>
       <BossItem handleButtonClickKilled={handleButtonClickKilled} handleButtonClickUndo={handleButtonClickUndo}/> 
+    </div> 
       <footer> 
         <p>Developed and Designed by Matt S. <span>This is an unofficial game companion for Elden Ring. The contents of this website are inspired by a video game and are not affiliated with or endorsed by the game's publisher or developer. All characters, images, and events depicted on this website are fictional and are not intended to depict real events or real persons. No copyright or trademark infringement is intended. This website is intended solely for entertainment and informational purposes.</span>   
         </p>
