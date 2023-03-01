ef8248079d846851301e1408536c8b37dd714d8a
Matt01270 <matt.92.online@googlemail.com>
2023-03-01 18:39:57 -0500

    removed local storage functionality and implemented progress bar count to firebase/database

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -2,9 +2,9 @@ const ProgressBar = (props) => {
     return( 
 
         <div className="progressContainer"> 
-          <div className='progressText'>Bosses slain: {props.changedBossCount} /112</div> 
+          <div className='progressText'>Bosses slain: {props.progressBarCount} /112</div> 
             <div className="progressBar"> 
-              <div className="progressBarFill" style={{width: `${(props.changedBossCount / 112) * 100}%`}}></div>
+              <div className="progressBarFill" style={{width: `${(props.progressBarCount / 112) * 100}%`}}></div>
             </div> 
         </div>
     )
